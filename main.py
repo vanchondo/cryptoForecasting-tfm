2d4afd8e3ca1edd4f7ec4e657d06a3b74b6e3fbe
vedannt <eduardoanchondo@gmail.com>
2022-09-23 14:34:15 -0600

    Adding ETH forecaster

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import atexit
 from apscheduler.schedulers.background import BackgroundScheduler
 from forecasters.BTCBacktestingForecaster import BTCBacktestingForecaster
+from forecasters.ETHBacktestingForecaster import ETHBacktestingForecaster
 from flask import Flask, jsonify, g
 
 app = Flask(__name__)
 btcForecaster = BTCBacktestingForecaster()
+ethForecaster = ETHBacktestingForecaster()
+
+days_ahead = 8
 
 @app.route('/predict/btc', methods=['GET'])
 def predictBTC():
-    high_pred = btcForecaster.predict(8, True).to_frame()
-    low_pred = btcForecaster.predict(8, False)
+    high_pred = btcForecaster.predict(days_ahead, True).to_frame()
+    low_pred = btcForecaster.predict(days_ahead, False)
     high_pred['low'] = low_pred
     response = []
     for row in high_pred.iterrows():
@@ -23,12 +27,34 @@ def predictBTC():
         })
         
     response.pop(0)
+
+    return jsonify(response)
+
+@app.route('/predict/eth', methods=['GET'])
+def predictETH():
+    high_pred = ethForecaster.predict(days_ahead, True).to_frame()
+    low_pred = ethForecaster.predict(days_ahead, False)
+    high_pred['low'] = low_pred
+    response = []
+    for row in high_pred.iterrows():
+        date = row[0].strftime('%Y-%m-%d')
+        high = row[1][0]
+        low = row[1][1]
+        response.append({
+            'Date' : date,
+            'High' : high,
+            'Low' : low
+        })
         
+    response.pop(0)
+
     return jsonify(response)
 
 def trainAll():
     btcForecaster.train(True) 
-    btcForecaster.train(False) 
+    btcForecaster.train(False)
+    ethForecaster.train(True)
+    ethForecaster.train(False)
 
 trainAll()
 scheduler = BackgroundScheduler()
